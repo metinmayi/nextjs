a8d47cef3c9b02c893e53438824a1e202e0bd0ef
Metin Ahmad <metinmayi@gmail.com>
2023-03-08 23:54:45 +0100

    Created skeleton for hamburger menu

---
+++ src/components/Navbar/HamburgerMenu/HamburgerMenu.tsx
@@ -0,0 +1,9 @@
+export default function HamburgerMenu() {
+  return (
+    <div className="flex flex-col justify-center p-5 gap-1">
+      <div className="w-10 h-1 bg-black"></div>
+      <div className="w-10 h-1 bg-black"></div>
+      <div className="w-10 h-1 bg-black"></div>
+    </div>
+  );
+}
