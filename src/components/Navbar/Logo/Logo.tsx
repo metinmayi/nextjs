be2a1483aa32b076e6384b2c1b8fc48a53696c1c
Metin Ahmad <metinmayi@gmail.com>
2023-03-09 00:01:20 +0100

    Added skeleton for Logo

---
+++ src/components/Navbar/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="flex items-center">
+      <img src="/site-logo.svg" alt="BeautyPoint-Logo" className="h-3/4" />
+    </div>
+  );
+}
