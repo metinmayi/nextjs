26cd0f25804274931a5e4846cf7a2017c417ff39
Metin Ahmad <metinmayi@gmail.com>
2023-03-09 08:59:20 +0100

    Updated Navbar with new components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,14 @@
+import BookingButton from "./BookingButton/BookingButton";
 import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
+import Logo from "./Logo/Logo";
 
 export default function Navbar() {
   return (
-    <nav className="absolute top-0 flex">
+    <nav className="fixed top-0 flex justify-between h-20 w-full">
       <HamburgerMenu />
+      {/* <Search /> */}
+      <Logo />
+      <BookingButton />
     </nav>
   );
 }
