7e11c1d80058b0d2f304e7a3ae42bea6bcdc025e
Metin Ahmad <metinmayi@gmail.com>
2023-03-10 03:21:02 +0100

    Adde animation when opening/closing

--- src/components/Navbar/NavbarHeader/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/Navbar/NavbarHeader/HamburgerMenu/HamburgerMenu.tsx
@@ -4,11 +4,11 @@ export default function HamburgerMenu() {
   return (
     <>
       <div
-        className="flex grow flex-col justify-center pl-5 gap-1"
+        className="flex grow flex-col justify-center pl-5 gap-1 transition-all duration-200"
         onClick={toggleNavbarContentVisibility}>
-        <div className="h-1 w-1/2 bg-black"></div>
-        <div className="h-1 w-1/2 bg-black"></div>
-        <div className="h-1 w-1/2 bg-black"></div>
+        <div className="h-1 w-1/2 bg-black hamburgerLine rotate-0 translate-y-0 transition-all duration-1000"></div>
+        <div className="h-1 w-1/2 bg-black hamburgerLine rotate-0 translate-y-0 transition-all duration-1000"></div>
+        <div className="h-1 w-1/2 bg-black hamburgerLine rotate-0 opacity-1 translate-y-0 transition-all duration-1000 ease-in-out"></div>
       </div>
     </>
   );
--- src/components/Navbar/NavbarHeader/HamburgerMenu/toggleNavbarContentVisibility.js
+++ src/components/Navbar/NavbarHeader/HamburgerMenu/toggleNavbarContentVisibility.js
@@ -4,4 +4,30 @@ export function toggleNavbarContentVisibility() {
   navbarContent.classList.toggle("pointer-events-none");
   navbarContent.classList.toggle("opacity-0");
   navbarContent.classList.toggle("-translate-y-full");
+  handleHamburgerLines();
+}
+
+function handleHamburgerLines() {
+  const [top, middle, bottom] = document.querySelectorAll(".hamburgerLine");
+
+  top.parentNode.classList.toggle("gap-1");
+  top.parentNode.classList.toggle("gap-0");
+
+  top.classList.toggle("rotate-0");
+  top.classList.toggle("rotate-45");
+  top.classList.toggle("translate-y-0");
+  top.classList.toggle("translate-y-1/2");
+
+  middle.classList.toggle("rotate-0");
+  middle.classList.toggle("-rotate-45");
+  middle.classList.toggle("translate-y-0");
+  middle.classList.toggle("-translate-y-1/2");
+
+  bottom.classList.toggle("opacity-1");
+  bottom.classList.toggle("opacity-0");
+  bottom.classList.toggle("h-1");
+  bottom.classList.toggle("rotate-0");
+  bottom.classList.toggle("-rotate-45");
+  bottom.classList.toggle("translate-y-0");
+  bottom.classList.toggle("-translate-y-1");
 }
